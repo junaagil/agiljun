1afe3bdc90e9dc4df8e78efbbd5cfb9752bf05e4
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-01 00:45:32 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-./ngrok authtoken 2KoYqyM0hKY5YJ5dUY47CjEMhBi_rRkr4ZeAVVGKtde6V2VC > /dev/null 2>&1
-echo region: us >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 22 >> ngrok.yml
