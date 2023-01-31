8dfb636415e5b44a6aa8dcf131a35348a4f2219c
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-01 00:43:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 ./ngrok authtoken 2KoYqyM0hKY5YJ5dUY47CjEMhBi_rRkr4ZeAVVGKtde6V2VC > /dev/null 2>&1
 echo region: us >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
-echo IP:
-curl -s localhost:4040/api/tunnels
+echo IP: localhost:4040
