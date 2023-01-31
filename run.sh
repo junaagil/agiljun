1708dda91aeddbdbfabeb80b867fba121d5a35d9
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-01 01:22:51 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
-echo root:$password | chpasswd
-mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
-echo "export LD_LIBRARY_PATH" >> /root/.bashrc
