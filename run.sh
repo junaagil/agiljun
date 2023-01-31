bab7a4af2acbb7e48ae57d05c380eab0c3548c7d
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-01 00:53:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sudo echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
 sudo echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 sudo service ssh start
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+./ngrok tcp 22 | curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
