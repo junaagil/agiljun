4d2fe49b5f1b9adb966c4f2e3c6befc623daeec7
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-02 20:42:57 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ sudo apt update -y
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
 sudo service ssh start
 ./ngrok authtoken 2KoYqyM0hKY5YJ5dUY47CjEMhBi_rRkr4ZeAVVGKtde6V2VC
-./ngrok tcp 22
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+./ngrok tcp 22 > /dev/null 2>&1 | curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo echo create root password
