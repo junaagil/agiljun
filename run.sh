0518aae0ca55c5473fa45d8e9a3c528de7f8eecf
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-02 20:32:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 sudo apt update -y
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
-sudo cat etc/ssh/sshd_config
+sudo service ssh start
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo echo create root password
