973f5a2b137a89d282246e6188d5648986e86bbe
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-02 21:15:48 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update -y
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
-sudo service ssh start
-./ngrok authtoken 2KoYqyM0hKY5YJ5dUY47CjEMhBi_rRkr4ZeAVVGKtde6V2VC
-./ngrok tcp 5900 --region=us
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
