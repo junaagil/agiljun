6a64e420e190378b9b9e5476462766b1019c8620
junaagil <124076502+junaagil@users.noreply.github.com>
2023-02-02 20:57:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ sudo apt update -y
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
 sudo service ssh start
 ./ngrok authtoken 2KoYqyM0hKY5YJ5dUY47CjEMhBi_rRkr4ZeAVVGKtde6V2VC
-echo proto: tcp >> ngrok.yml & echo addr: 3389 >> ngrok.yml
+echo proto: tcp >> ngrok.yml && echo addr: 3389 >> ngrok.yml
